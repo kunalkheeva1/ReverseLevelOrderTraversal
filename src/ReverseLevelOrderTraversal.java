a1ad7d5234dd62335bd08fb25a9f960995c3a1c5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 22:36:48 -0700

    coded a method to return the reverse of level order traversal level wise from left to right.

--- src/ReverseLevelOrderTraversal.java
+++ src/ReverseLevelOrderTraversal.java
@@ -1,4 +1,7 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Node {
     int data;
@@ -14,6 +17,24 @@ class Node {
 
 public class ReverseLevelOrderTraversal {
 
+    static ArrayList<Integer> reverseLevelOrderTraversal(Node node){
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(node == null) return ans;
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()){
+            Node current = q.remove();
+            ans.add(current.data);
+            if(current.right != null){
+                q.add(current.right);
+            }
+            if(current.left != null){
+                q.add(current.left);
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
 
 
     public static void main(String[] args) {
