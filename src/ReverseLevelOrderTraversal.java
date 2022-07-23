265a68a1c5cf52ecc3d694565270912c14591ebc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 22:40:24 -0700

    comments added for the better understanding

--- src/ReverseLevelOrderTraversal.java
+++ src/ReverseLevelOrderTraversal.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
+//TreeNode
 class Node {
     int data;
     Node left;
@@ -16,15 +17,19 @@ class Node {
 
 
 public class ReverseLevelOrderTraversal {
-
+//method to reverse the levels from left to right
     static ArrayList<Integer> reverseLevelOrderTraversal(Node node){
         ArrayList<Integer> ans = new ArrayList<>();
         if(node == null) return ans;
         Queue<Node> q = new LinkedList<>();
         q.add(node);
+        //until the queue gets emptied
         while(!q.isEmpty()){
+            //remove the node form queue and add its data in list
             Node current = q.remove();
             ans.add(current.data);
+            //then go to its right side first, as by this way, at the end we will have a list to be reversed
+            // to get the desired answer
             if(current.right != null){
                 q.add(current.right);
             }
